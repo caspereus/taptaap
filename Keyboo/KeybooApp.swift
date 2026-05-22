2eaf91e50aa9f0db941edfd673ed0a67ebe358ba
caspereus <aldy.iputu222@gmail.com>
2026-05-22 17:21:08 +0700

    Initial Commit

---
+++ Keyboo/KeybooApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeybooApp.swift
+//  Keyboo
+//
+//  Created by Aldy on 22/05/26.
+//
+
+import SwiftUI
+
+@main
+struct KeybooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
